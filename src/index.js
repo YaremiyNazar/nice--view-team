00ccdb093fa4f979001ca9c4bc15863dcf027953
Your Name <yaremiy.nazar@gmail.com>
2023-03-16 15:14:07 +0200

    project

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './js/mobile-menu';
+import './js/gallery';
+import './js/modal';
+import './js/modal-flat';
+import './js/download-modal';
+import './js/btn-up';
---
+++ src/js/gallery.js
@@ -0,0 +1,44 @@
+import Swiper, { Navigation, Pagination, EffectCoverflow } from 'swiper';
+
+const swiper = new Swiper('.mySwiper', {
+  // configure Swiper to use modules
+  modules: [Navigation, Pagination, EffectCoverflow],
+  // Optional parameters
+  centeredSlides: true,
+  slidesPerView: 2,
+  spaceBetween: 0,
+  loop: true,
+  grabCursor: true,
+  slideToClickedSlide: true,
+
+  speed: 800,
+
+  effect: 'coverflow',
+  coverflowEffect: {
+    rotate: 15,
+    stretch: 32,
+    scale: 0.6,
+    slideShadows: false,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    enabled: false,
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    768: {
+      navigation: {
+        enabled: true,
+      },
+    },
+  },
+});
